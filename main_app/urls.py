9a36062eb4a99b36a5a94095177f55f31acc9d40
Brian Lusina <lusinabrian@gmail.com>
2016-09-10 22:05:37 +0300

    Add detail template

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(regex=r'^$', view=views.index)
+    url(regex=r'^$', view=views.index, name="index"),
+    url(regex=r'^/([0-9]+)/$', view=views.detail, name="detail")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from .models import Treasures
 
 
-# Create your views here.
+# home page for the main app
 def index(request):
     treasures = Treasures.objects.all()
     context = {"treasures": treasures}
     return render(request=request, template_name="index.html", context=context)
+
+
+# displays the detail view for each treasure
+def detail(request, treasure_id):
+    context = {"treasure": Treasures.objects.get(id=treasure_id)}
+    render(request=request, template_name='detail.html', context=context)
+
