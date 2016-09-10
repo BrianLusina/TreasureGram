ff8951399f8e8fe581e9d7b99ae6c0423e3716e9
Brian Lusina <lusinabrian@gmail.com>
2016-09-10 17:55:02 +0300

    update settings

--- TreasureGram/settings.py
+++ TreasureGram/settings.py
@@ -124,7 +124,8 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+	BASE_DIR + "main_app/static"
+#    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
