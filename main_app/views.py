18bc7dc737e52f19f68d920269cc3ee5555a21f1
Brian Lusina <lusinabrian@gmail.com>
2016-09-10 10:08:35 +0300

    Create Treasure class

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    name = "Gold Nugget"
-    value = 1000.00
-    context = {'treasure_name': name,
-               'treasure_val': value}
-    return render(request=request, template_name="index.html", context=context)
+    context = {'treasures': treasures}
+    return render(request=request, template_name="index.html",context=context)
+
+
+class Treasures(object):
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+
+treasures = [
+    Treasures("Gold Nugget", 1000, 'gold', 'Curlys creek, NM'),
+    Treasures("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
+    Treasures("Coffee Can", 20, 'tin', 'ACME, CA'),
+    Treasures("Diamond", 5000, 'diamond', 'Peak Valley, NO'),
+]
