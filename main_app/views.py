77d56970578c4c5fd4d816db22629afdcf26baa4
Brian Lusina <lusinabrian@gmail.com>
2016-09-10 10:12:00 +0300

    Add for loop to display all data

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {'treasures': treasures}
-    return render(request=request, template_name="index.html",context=context)
+    return render(request=request, template_name="index.html", context=context)
 
 
 class Treasures(object):
