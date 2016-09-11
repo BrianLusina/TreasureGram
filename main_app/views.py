500607db6c15baedde40934eaf66ad83900784ea
Brian Lusina <lusinabrian@gmail.com>
2016-09-12 00:16:37 +0300

    Add post_treasure url

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .models import Treasures
+from .forms import TreasureForm
 
 
 # home page for the main app
@@ -14,3 +15,22 @@ def detail(request, treasure_id):
     context = {"treasure": Treasures.objects.get(id=treasure_id)}
     return render(request=request, template_name='detail.html', context=context)
 
+
+def post_treasure(request):
+    """
+    Create a form and link it to the posted data
+    check if form is valid then clean the data before saving the data in the database
+    Redirect to home page even if the form is not valid.
+    :param request:
+    :return:
+    """
+    form = TreasureForm(request.POST)
+    # check if the form is valid
+    if form.is_valid():
+        treasure = Treasures(name=form.cleaned_data['name'],
+                             value=form.cleaned_data['value'],
+                             material=form.cleaned_data['material'],
+                             location=form.cleaned_data['location'],
+                             img_url=form.cleaned_data['img_url'])
+        treasure.save()
+    return HttpResponseRedirect('/')
