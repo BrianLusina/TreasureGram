a553ace25a9cf34f67a6222f1ea65602846f3109
Brian Lusina <lusinabrian@gmail.com>
2016-09-12 00:46:54 +0300

    reading data from forms

--- main_app/views.py
+++ main_app/views.py
@@ -28,10 +28,13 @@ def post_treasure(request):
     form = TreasureForm(request.POST)
     # check if the form is valid
     if form.is_valid():
-        treasure = Treasures(name=form.cleaned_data['name'],
-                             value=form.cleaned_data['value'],
-                             material=form.cleaned_data['material'],
-                             location=form.cleaned_data['location'],
-                             img_url=form.cleaned_data['img_url'])
-        treasure.save()
+        # reads all the form data and saves it to the database
+        form.save(commit=True)
+        # altenatively, if subclassing TreasureForm to Form
+        # treasure = Treasures(name=form.cleaned_data['name'],
+        #                      value=form.cleaned_data['value'],
+        #                      material=form.cleaned_data['material'],
+        #                      location=form.cleaned_data['location'],
+        #                      img_url=form.cleaned_data['img_url'])
+        #treasure.save()
     return HttpResponseRedirect('/')
