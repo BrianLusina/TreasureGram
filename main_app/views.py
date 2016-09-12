dd35ac07707af985b4dc8a9630f4efc0c56afbfa
Brian Lusina <lusinabrian@gmail.com>
2016-09-12 09:32:35 +0300

    Update like_treasure function

--- main_app/views.py
+++ main_app/views.py
@@ -122,4 +122,5 @@ def like_treasure(request):
             likes = treasure.likes + 1
             treasure.likes = likes
             treasure.save()
-        return HttpResponse(likes)
+
+    return HttpResponse(likes)
